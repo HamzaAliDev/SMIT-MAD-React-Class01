38cd509301088fc5bfe0f6bc2dfda95791b0712b
Ali Hamza <hamzamashooq492@gmail.com>
2024-07-03 18:39:14 +0500

    Basic Structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+            <div className="container-fluid ">
+                <div className="row">
+                    <div className="col bg-dark text-white text-center pt-3">
+                        <p>&copy; 2024. All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
